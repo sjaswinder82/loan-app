b9635a81474681370db9c1069d570fe983c93ddb
Jaswinder Singh <sjeswinder@jstrainer.com>
2022-02-15 00:10:28 +0530

    Updated test case for logged user

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,17 +2,42 @@
 
 namespace Tests;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 use Illuminate\Support\Facades\Artisan;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication, DatabaseMigrations;
 
+    protected $token;
+
     public function setUp(): void
     {
         parent::setUp();
-        // Artisan::call('jwt:secret');
+    }
+
+    /**
+     * Set the currently logged in user for the application.
+     *
+     * @param  \Illuminate\Contracts\Auth\Authenticatable $user
+     * @param  string|null                                $driver
+     * @return $this
+     */
+    public function actingAs($user=null, $driver = null)
+    {
+        if(!$user) {
+            $user =  User::factory()->create([
+                'password' => bcrypt($password = '123456'),
+            ]);
+        }       
+        
+        $token = JWTAuth::fromUser($user);
+        $this->withHeader('Authorization', "Bearer {$token}");
+        parent::actingAs($user);
+        
+        return $this;
     }
 }
